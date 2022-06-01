f66610c4f31a1ca76c6d9afd86984d485cb58013
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-06-01 11:33:24 -0400

    markdown added

---
+++ index.js
@@ -0,0 +1,18 @@
+// DEPENDENCIES
+// TODO: Include packages needed for this application
+const markdown = require("generateMarkdown.js");
+
+// STATE - DATA
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// FUNCTIONS
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// INITIALIZATION
+// Function call to initialize app
+init();
