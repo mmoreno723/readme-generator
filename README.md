2881c43deb6460ef36ffabed6749b9702788ddfb
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-06-02 12:34:46 -0400

    fixed readme codeblock issue

---
+++ README.md
@@ -0,0 +1,38 @@
+# cool title
+![GitHub License] (url)
+    
+## Description
+    
+Github: morenom
+a cool project
+
+## Table of Contents
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+- [License](#license)
+
+- [Contributing](#contributing)
+
+- [Tests](#tests)
+
+- [Questions](#questions)
+
+## Installation
+npm i
+
+## Usage
+nothing
+
+## License
+
+## Contributing
+nothing
+
+## Tests
+npm test
+
+## Questions
+If you have any questions please contact me at mmoreno@email
--- index.js
+++ index.js
@@ -1,11 +1,133 @@
 // DEPENDENCIES
 // TODO: Include packages needed for this application
-const markdown = require("generateMarkdown.js");
+const markdown = require("./generateMarkdown");
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // STATE - DATA
-// TODO: Create an array of questions for user input
-const questions = [];
 
+const generateReadme = ({
+  github,
+  email,
+  title,
+  description,
+  dependencies,
+  tests,
+  usage,
+  contribution,
+}) => {
+  const readme = `# ${title}
+![GitHub License] (url)
+    
+## Description
+    
+Github: ${github}
+${description}
+
+## Table of Contents
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+- [License](#license)
+
+- [Contributing](#contributing)
+
+- [Tests](#tests)
+
+- [Questions](#questions)
+
+## Installation
+${dependencies}
+
+## Usage
+${usage}
+
+## License
+
+## Contributing
+${contribution}
+
+## Tests
+${tests}
+
+## Questions
+If you have any questions please contact me at ${email}`;
+  console.log(readme);
+  fs.writeFile("README.md", readme, "utf8", (err) => {
+    if (err) throw err;
+    console.log("successful!");
+  });
+};
+// TODO: Create an array of questions for user input
+const questions = [
+  "What is your GitHub username?",
+  "What is your email address?",
+  "What is the title of your project?",
+  "Provide a short description of your project",
+  "What kind of license should your project have?",
+  "What command should be run to install dependencies?",
+  "What command should be run to run tests?",
+  "What does the user need to know about using the repo?",
+  "What does the user need to know about contributing to the repo?",
+];
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: questions[0],
+      name: "github",
+    },
+    {
+      type: "input",
+      message: questions[1],
+      name: "email",
+    },
+    {
+      type: "input",
+      message: questions[2],
+      name: "title",
+    },
+    {
+      type: "input",
+      message: questions[3],
+      name: "description",
+    },
+    {
+      type: "input",
+      message: questions[4],
+      name: "license",
+    },
+    {
+      type: "input",
+      message: questions[5],
+      name: "dependencies",
+    },
+    {
+      type: "input",
+      message: questions[6],
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: questions[7],
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: questions[8],
+      name: "contribution",
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+    generateReadme(response);
+    fs.writeFile("README.md", ``, (err) => {
+      err ? console.log(err) : console.log("Success!");
+    });
+  });
 // FUNCTIONS
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
